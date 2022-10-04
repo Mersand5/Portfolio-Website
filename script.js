5791095d3276ae87c05ee21f3b7cd50f88965d5a
lenny dimet <lennydimet@gmail.com>
2022-10-04 11:07:27 +0100

    adding flip functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+const card = document.querySelector('.card_inner');
+
+card.addEventListener('click', () => {
+        card.classList.toggle('is-flipped');
+    });
+
+const card2 = document.querySelector('.card_inner2');
+
+    card2.addEventListener('click', () => {
+            card2.classList.toggle('is-flipped2');
+    });
+const card3 = document.querySelector('.card_inner3');
+
+card3.addEventListener('click', () => {
+    card3.classList.toggle('is-flipped3');
+     });
+          
